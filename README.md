721711f83629559196cffb65349eac8721062f68
Welton <wel.indumill@gmail.com>
2022-12-08 17:48:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# aprendendoJava
+ meuPortifolio
---
+++ asserts/javascripts/main.js
@@ -0,0 +1,9 @@
+var dece = document.getElementById("menujava");
+var resultado = document.getElementById("bt");
+
+function clique(){
+    resultado.nodeValue();
+    if (resultado == true){
+        dece.onfocus
+    }
+};
